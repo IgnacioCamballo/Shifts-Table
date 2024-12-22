a2a1e9dabca01c0acd79c4fea4b1bc2486113432
Ignacio Camballo <ignaciocamballo@gmail.com>
2024-12-22 12:46:58 +0000

    will try to change to expo settings

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -43,11 +43,11 @@ const theme = {
         // detail: "ca-app-pub-4926030013898312/4565563096",
         // shifts: "ca-app-pub-4926030013898312/8504808104",
         // totals: "ca-app-pub-4926030013898312/7191726433"
-        //the bottom ones are test ads
-        calendar: "ca-app-pub-3940256099942544/9214589741",
-        detail: "ca-app-pub-3940256099942544/9214589741",
-        shifts: "ca-app-pub-3940256099942544/9214589741",
-        totals: "ca-app-pub-3940256099942544/9214589741"
+        // the bottom ones are test ads
+         calendar: "ca-app-pub-3940256099942544/9214589741",
+         detail: "ca-app-pub-3940256099942544/9214589741",
+         shifts: "ca-app-pub-3940256099942544/9214589741",
+         totals: "ca-app-pub-3940256099942544/9214589741"
     }
 }
 
